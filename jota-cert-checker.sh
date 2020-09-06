411b79907c7475c67edf4a61df8c9e72398f5f36
Cem Sagan <cemsagan@gmail.com>
2020-09-06 21:28:23 +0100

    Update jota-cert-checker.sh
    calculate func added
    certificate_last_day statement changed
    css classes are used for table styles
    code prettify

--- jota-cert-checker.sh
+++ jota-cert-checker.sh
@@ -14,14 +14,17 @@
 #
 
 sites_list="$1"
-sitename=""
 html_file="certs_check.html"
+
+sitename=""
 current_date=$(date +%s)
 end_date=""
 days_left=""
 certificate_last_day=""
+
 warning_days="30"
 alert_days="15"
+
 # Terminal colors
 ok_color="\e[38;5;40m"
 warning_color="\e[38;5;220m"
@@ -33,62 +36,118 @@ end_of_color="\033[0m"
 # FUNCTIONS
 #
 
-html_mode(){
+calculate()
+{
+	sitename=$(echo $1 | cut -d ":" -f1)
+	
+	certificate_last_day=$(date -d "$(echo | openssl s_client -connect ${sitename}:443 2>/dev/null | openssl x509 -noout -dates|grep notAfter|cut -d= -f2)" +%Y-%m-%d)
+
+	end_date=$(date +%s -d "$certificate_last_day")
+
+	days_left=$(((end_date - current_date) / 86400))
+}
+
+html_mode()
+{
 	# Generate and reset file
 	cat <<- EOF > $html_file
 	<!DOCTYPE html>
 	<html>
 			<head>
-			<title>SSL Certs expiration</title>
+			<title>SSL Certs Expiration Report</title>
+			<style>
+				body {
+					background-color: white;
+					font-family: 'Arial', sans-serif;
+				}
+				h1 {
+					color: #253858;
+					text-align: center;
+					font-size: 20px;
+					font-weight: bold;
+				}
+				table {
+					background-color: #efefef;
+					padding: 10px;
+					font-size: 14px;
+					font-family: monospace;
+					margin-left: auto;
+					margin-right: auto;
+					border: 1px solid #ccc;
+					border-radius: 6px;
+					min-width: 600px;
+				}
+				tr {
+					padding: 8px;
+					text-align: left;
+				}
+				th {
+					 color: #253858;
+					 padding: 0 30px 8px 0;
+					 text-align: left;
+					 font-weight: bold;
+				}
+				td {
+					padding: 8px;
+				}
+				tr.ok > td {
+					background-color: #7DC242;
+				}
+				tr.alert > td {
+					background-color: #FF8F32;
+				}
+				tr.warning > td {
+					background-color: #FFE032;
+				}
+				tr.expired > td {
+					background-color: #EB655A;
+				}
+			</style>
 			</head>
-			<body style="background-color: lightblue;">
-					<h1 style="color: navy;text-align: center;font-family: 'Helvetica Neue', sans-serif;font-size: 20px;font-weight: bold;">SSL Certs expiration checker</h1>
-					<table style="background-color: #C5E1E7;padding: 10px;box-shadow: 5px 10px 18px #888888;margin-left: auto ;margin-right: auto ;border: 1px solid black;">
-					<tr style="padding: 8px;text-align: left;font-family: 'Helvetica Neue', sans-serif;">
-					<th style="padding: 8px;text-align: left;font-family: 'Helvetica Neue', sans-serif;font-weight: bold;">Site</th>
-					<th style="padding: 8px;text-align: left;font-family: 'Helvetica Neue', sans-serif;font-weight: bold;">Expiration date</th>
-					<th style="padding: 8px;text-align: left;font-family: 'Helvetica Neue', sans-serif;font-weight: bold;">Days left</th>
-					<th style="padding: 8px;text-align: left;font-family: 'Helvetica Neue', sans-serif;font-weight: bold;">Status</th>
+			<body>
+					<h1>SSL Certs Expiration Report</h1>
+					<table>
+					<tr>
+					<th>Site</th>
+					<th>Expiration Date</th>
+					<th>Days Left</th>
+					<th>Status</th>
 					</tr>
 	EOF
 
 	while read site;do
-		sitename=$(echo $site | cut -d ":" -f1)
-		certificate_last_day=$(echo | openssl s_client -servername ${sitename} -connect ${site} 2>/dev/null | \
-		openssl x509 -noout -enddate 2>/dev/null | cut -d "=" -f2)
-		end_date=$(date +%s -d "$certificate_last_day")
-		days_left=$(((end_date - current_date) / 86400))
+		calculate $site
 
 		if [ "$days_left" -gt "$warning_days" ];then
-			echo "<tr style=\"padding: 8px;text-align: left;font-family: 'Helvetica Neue', sans-serif;\">" >> $html_file
-			echo "<td style=\"padding: 8px;background-color: #33FF4F;\">${sitename}</td>" >> $html_file
-			echo "<td style=\"padding: 8px;background-color: #33FF4F;\">${certificate_last_day}</td>" >> $html_file
-			echo "<td style=\"padding: 8px;background-color: #33FF4F;\">${days_left}</td>" >> $html_file
-			echo "<td style=\"padding: 8px;background-color: #33FF4F;\">Ok</td>" >> $html_file
+			echo "<tr class=\"ok\">" >> $html_file
+			echo "<td>${sitename}</td>" >> $html_file
+			echo "<td>${certificate_last_day}</td>" >> $html_file
+			echo "<td>${days_left}</td>" >> $html_file
+			echo "<td>OK</td>" >> $html_file
 			echo "</tr>" >> $html_file
 
 		elif [ "$days_left" -le "$warning_days" ] && [ "$days_left" -gt "$alert_days" ];then
-			echo "<tr style=\"padding: 8px;text-align: left;font-family: 'Helvetica Neue', sans-serif;\">" >> $html_file
-			echo "<td style=\"padding: 8px;background-color: #FFE032;\">${sitename}</td>" >> $html_file
-			echo "<td style=\"padding: 8px;background-color: #FFE032;\">${certificate_last_day}</td>" >> $html_file
-			echo "<td style=\"padding: 8px;background-color: #FFE032;\">${days_left}</td>" >> $html_file
-			echo "<td style=\"padding: 8px;background-color: #FFE032;\">Warning</td>" >> $html_file
+			echo "<tr class=\"warning\">" >> $html_file
+			echo "<td>${sitename}</td>" >> $html_file
+			echo "<td>${certificate_last_day}</td>" >> $html_file
+			echo "<td>${days_left}</td>" >> $html_file
+			echo "<td>Warning</td>" >> $html_file
 			echo "</tr>" >> $html_file
 
 		elif [ "$days_left" -le "$alert_days" ] && [ "$days_left" -gt 0 ];then
-			echo "<tr style=\"padding: 8px;text-align: left;font-family: 'Helvetica Neue', sans-serif;\">" >> $html_file
-			echo "<td style=\"padding: 8px;background-color: #FF8F32;\">${sitename}</td>" >> $html_file
-			echo "<td style=\"padding: 8px;background-color: #FF8F32;\">${certificate_last_day}</td>" >> $html_file
-			echo "<td style=\"padding: 8px;background-color: #FF8F32;\">${days_left}</td>" >> $html_file
-			echo "<td style=\"padding: 8px;background-color: #FF8F32;\">Alert</td>" >> $html_file
+			echo "<tr class=\"alert\">" >> $html_file
+			echo "<td>${sitename}</td>" >> $html_file
+			echo "<td>${certificate_last_day}</td>" >> $html_file
+			echo "<td>${days_left}</td>" >> $html_file
+			echo "<td>Alert</td>" >> $html_file
 			echo "</tr>" >> $html_file
 
 		elif [ "$days_left" -le 0 ];then
-			echo "<tr style=\"padding: 8px;text-align: left;font-family: 'Helvetica Neue', sans-serif;\">" >> $html_file
-			echo "<td style=\"padding: 8px;background-color: #EF3434;\">${sitename}</td>" >> $html_file
-			echo "<td style=\"padding: 8px;background-color: #EF3434;\">${certificate_last_day}</td>" >> $html_file
-			echo "<td style=\"padding: 8px;background-color: #EF3434;\">${days_left}</td>" >> $html_file
-			echo "<td style=\"padding: 8px;background-color: #EF3434;\">Expired</td>" >> $html_file
+			echo "<tr class=\"expired\">" >> $html_file
+			echo "<td>${sitename}</td>" >> $html_file
+			echo "<td>${certificate_last_day}</td>" >> $html_file
+			echo "<td>${days_left}</td>" >> $html_file
+			echo "<td>Expired</td>" >> $html_file
 			echo "</tr>" >> $html_file
 
 		fi
@@ -102,19 +161,17 @@ html_mode(){
 	EOF
 }
 
-terminal_mode(){
+terminal_mode()
+{
 	printf "\n| %-30s | %-30s | %-10s | %-5s %s\n" "SITE" "EXPIRATION DAY" "DAYS LEFT" "STATUS"
 
 	while read site;do
-		sitename=$(echo $site | cut -d ":" -f1)
-		certificate_last_day=$(echo | openssl s_client -servername ${sitename} -connect ${site} 2>/dev/null | \
-		openssl x509 -noout -enddate 2>/dev/null | cut -d "=" -f2)
-		end_date=$(date +%s -d "$certificate_last_day")
-		days_left=$(((end_date - current_date) / 86400))
+
+		calculate $site
 
 		if [ "$days_left" -gt "$warning_days" ];then
 			printf "${ok_color}| %-30s | %-30s | %-10s | %-5s %s\n${end_of_color}" \
-			"$sitename" "$certificate_last_day" "$days_left" "Ok"
+			"$sitename" "$certificate_last_day" "$days_left" "OK"
 
 		elif [ "$days_left" -le "$warning_days" ] && [ "$days_left" -gt "$alert_days" ];then
 			printf "${warning_color}| %-30s | %-30s | %-10s | %-5s %s\n${end_of_color}" \
@@ -132,13 +189,14 @@ terminal_mode(){
 	done < $sites_list
 
 	printf "\n %-10s" "STATUS LEGEND"
-	printf "\n ${ok_color}%-8s${end_of_color} %-30s" "Ok" "- More than ${warning_days} days left until the certificate expires"
+	printf "\n ${ok_color}%-8s${end_of_color} %-30s" "OK" "- More than ${warning_days} days left until the certificate expires"
 	printf "\n ${warning_color}%-8s${end_of_color} %-30s" "Warning" "- The certificate will expire in less than ${warning_days} days"
 	printf "\n ${alert_color}%-8s${end_of_color} %-30s" "Alert" "- The certificate will expire in less than ${alert_days} days"
 	printf "\n ${expired_color}%-8s${end_of_color} %-30s\n\n" "Expired" "- The certificate has already expired"
 }
 
-howtouse(){
+howtouse()
+{
 	cat <<-'EOF'
 
 	You must always specify -f option with the name of the file that contains the list of sites to check
